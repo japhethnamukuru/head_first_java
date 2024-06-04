e555d408dc46e8bbdb81961f7fca4e377cb1e23f
Japheth <japhethnamukuru@gmail.com>
2024-06-04 12:41:50 +0300

    Testing real vs driver (tester class)

---
+++ Cat.java
@@ -0,0 +1,16 @@
+package demo;
+
+// Real Cat class
+
+public class Cat {
+    
+    // Declaring attributes(states)
+    int size;
+    String breed;
+    String name;
+    
+    // Defining methods (behaviour)
+    void catMeow() {
+        System.out.println("Meow Meow");
+    }
+}
---
+++ CatDriver.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class CatDriver {
+    public static void main(String [] args) {
+        Cat cat = new Cat();
+        
+        cat.size = 20;
+        cat.breed = "Russian";
+        cat.name = "Jane";
+        
+        System.out.println("My name is " + cat.name + ", my breed is " + cat.breed + " and I am " + cat.size + " cm long");
+    }
+}
